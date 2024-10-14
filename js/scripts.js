4e6fa01fcdad896bbcd60ba73ab2eaad32adf24a
ciudaddeleganes <clubdeportivo.ciudaddeleganes@gmail.com>
2024-10-14 15:38:43 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+document.getElementById("toggle").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+});
+
+const images = [
+    "images/foto.webp",
+    "images/foto.webp",
+    "images/foto.webp",
+];
+
+let currentIndex = 0;
+
+const galleryImage = document.getElementById("galleryImage");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+function updateImage() {
+    galleryImage.src = images[currentIndex];
+}
+
+prevButton.addEventListener("click", () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+    updateImage();
+});
+
+nextButton.addEventListener("click", () => {
+    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+    updateImage();
+});
+
+// Desplazamiento automático cada 3 segundos
+setInterval(() => {
+    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+    updateImage();
+}, 10000);
